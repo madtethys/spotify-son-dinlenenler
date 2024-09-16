2d2c36b3f20cbb7ad5079cab8c0bde642a6c58c4
Mustafa Arda Düşova <info@madgrafik.com>
2024-09-16 14:23:00 +0300

    Ayarlar Sekmesi Güncellemesi

--- components/MarkdownSnippet.tsx
+++ components/MarkdownSnippet.tsx
@@ -1,4 +1,4 @@
-import { Input, Space, Typography, Tabs, Slider, Switch } from 'antd';
+import { Input, Space, Typography, Tabs, Switch, InputNumber } from 'antd';
 import React, { useState } from 'react';
 import * as Constants from '../utils/Constants';
 
@@ -13,9 +13,9 @@ interface Props {
 
 export default function MarkdownSnippet(props: Props): JSX.Element | null {
     const { username, theme } = props;
-    const [trackCount, setTrackCount] = useState<number>(5); // Varsayılan değeri 5
-    const [width, setWidth] = useState<number>(400); // Varsayılan değeri 400
-    const [uniqueTracks, setUniqueTracks] = useState<boolean>(false); // Varsayılan değeri hayır
+    const [trackCount, setTrackCount] = useState<number>(5);
+    const [width, setWidth] = useState<number>(400);
+    const [uniqueTracks, setUniqueTracks] = useState<boolean>(false);
 
     if (!username) {
         return null;
@@ -23,89 +23,82 @@ export default function MarkdownSnippet(props: Props): JSX.Element | null {
 
     const svgSrc = `${Constants.BaseUrl}/api?user=${username}`;
     const updateParams = `&count=${trackCount}&width=${width}${uniqueTracks ? '&unique=true' : ''}`;
-    const markdownCode = `![Spotify Son Dinlenen Müzikler](${svgSrc}${updateParams})`;
-    const htmlCode = `<img src="${svgSrc}${updateParams}" alt="Spotify Son Dinlenen Müzikler - Mustafa Arda Düşova" />`;
+    const finalUrl = `${svgSrc}${updateParams}`;
+    const markdownCode = `![Spotify Son Dinlenen Müzikler](${finalUrl})`;
+    const htmlCode = `<img src="${finalUrl}" alt="Spotify Son Dinlenen Müzikler - Mustafa Arda Düşova" />`;
 
     return (
-        <Tabs defaultActiveKey="1">
-            <TabPane tab="Markdown'a Nasıl Eklerim?" key="1">
-                <Space className="vert-space" direction="vertical" size="small">
-                    <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>
-                        "{username}" olarak giriş yapıldı.
-                    </Title>
-                    <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Varsayılan Markdown Kodu:</Text>
-                    <TextArea
-                        className="markdown"
-                        autoSize
-                        readOnly
-                        value={markdownCode}
-                        style={{
-                            backgroundColor: theme === 'dark' ? '#333333' : '#ffffff',
-                            color: theme === 'dark' ? '#ffffff' : '#000000'
-                        }}
-                    />
-                </Space>
-            </TabPane>
-            <TabPane tab="HTML'e Nasıl Eklerim?" key="2">
-                <Space className="vert-space" direction="vertical" size="small">
-                    <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>
-                        "{username}" olarak giriş yapıldı.
-                    </Title>
-                    <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Varsayılan HTML Kodu:</Text>
-                    <TextArea
-                        className="htmlkodu"
-                        autoSize
-                        readOnly
-                        value={htmlCode}
-                        style={{
-                            backgroundColor: theme === 'dark' ? '#333333' : '#ffffff',
-                            color: theme === 'dark' ? '#ffffff' : '#000000'
-                        }}
-                    />
-                </Space>
-            </TabPane>
-            <TabPane tab="Ayarlar" key="3">
-                <Space className="vert-space" direction="vertical" size="small">
-                    <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>
-                        Ayarları Yapılandırın
-                    </Title>
-                    <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Listede bulunacak müzik sayısını ayarlayın:</Text>
-                    <Slider
-                        min={1}
-                        max={10}
-                        step={1}
-                        value={trackCount}
-                        onChange={(value: number | [number, number]) => {
-                            // Slider değeri number ise setTrackCount ile güncelle
-                            if (typeof value === 'number') {
-                                setTrackCount(value);
-                            }
-                        }}
-                        marks={{ 1: '1', 10: '10' }}
-                    />
-                    <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Listenin genişliğini ayarlayın (px):</Text>
-                    <Slider
-                        min={300}
-                        max={1000}
-                        step={1}
-                        value={width}
-                        onChange={(value: number | [number, number]) => {
-                            // Slider değeri number ise setWidth ile güncelle
-                            if (typeof value === 'number') {
-                                setWidth(value);
-                            }
-                        }}
-                        marks={{ 300: '300px', 1000: '1000px' }}
-                    />
-                    <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Listede tekrar dinlenen müzikleri göster:</Text>
-                    <Switch
-                        checked={uniqueTracks}
-                        onChange={checked => setUniqueTracks(checked)}
-                        checkedChildren="Evet"
-                        unCheckedChildren="Hayır"
-                    />
-                </Space>
-            </TabPane>
-        </Tabs>
+        <div>
+            <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#000000', marginBottom: '20px' }}>
+                "{username}" olarak giriş yapıldı.
+            </Title>
+            <Tabs defaultActiveKey="1">
+                <TabPane tab="Markdown'a Nasıl Eklerim?" key="1">
+                    <Space className="vert-space" direction="vertical" size="small">
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Varsayılan Markdown Kodu:</Text>
+                        <TextArea
+                            className="markdown"
+                            autoSize
+                            readOnly
+                            value={markdownCode}
+                            style={{
+                                backgroundColor: theme === 'dark' ? '#333333' : '#ffffff',
+                                color: theme === 'dark' ? '#ffffff' : '#000000'
+                            }}
+                        />
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Önizleme:</Text>
+                        <object type="image/svg+xml" data={finalUrl} style={{ width: '100%', height: 'auto' }}></object>
+                    </Space>
+                </TabPane>
+                <TabPane tab="HTML'e Nasıl Eklerim?" key="2">
+                    <Space className="vert-space" direction="vertical" size="small">
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Varsayılan HTML Kodu:</Text>
+                        <TextArea
+                            className="htmlkodu"
+                            autoSize
+                            readOnly
+                            value={htmlCode}
+                            style={{
+                                backgroundColor: theme === 'dark' ? '#333333' : '#ffffff',
+                                color: theme === 'dark' ? '#ffffff' : '#000000'
+                            }}
+                        />
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Önizleme:</Text>
+                        <object type="image/svg+xml" data={finalUrl} style={{ width: '100%', height: 'auto' }}></object>
+                    </Space>
+                </TabPane>
+                <TabPane tab="Ayarlar" key="3">
+                    <Space className="vert-space" direction="vertical" size="small">
+                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>
+                            Ayarları Yapılandırın
+                        </Title>
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Listede bulunacak müzik sayısını ayarlayın:</Text>
+                        <InputNumber
+                            min={1}
+                            max={10}
+                            value={trackCount}
+                            onChange={(value: number | null) => setTrackCount(value || 1)}
+                            style={{ width: '100%' }}
+                        />
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Listenin genişliğini ayarlayın (px):</Text>
+                        <InputNumber
+                            min={300}
+                            max={1000}
+                            step={1}
+                            value={width}
+                            onChange={(value: number | null) => setWidth(value || 300)}
+                            style={{ width: '100%' }}
+                        />
+                        <Text style={{ color: theme === 'dark' ? '#ffffff' : '#000000' }}>Listede tekrar dinlenen müzikleri göster:</Text>
+                        <Switch
+                            checked={uniqueTracks}
+                            onChange={checked => setUniqueTracks(checked)}
+                            checkedChildren="Evet"
+                            unCheckedChildren="Hayır"
+                        />
+                    </Space>
+                </TabPane>
+            </Tabs>
+        </div>
     );
 }
