c875c421008b91b58c94adce318f7edeed712463
Mustafa Arda Düşova <info@madgrafik.com>
2024-09-17 14:18:16 +0300

    Update MarkdownSnippet.tsx

--- components/MarkdownSnippet.tsx
+++ components/MarkdownSnippet.tsx
@@ -47,7 +47,6 @@ export default function MarkdownSnippet(props: Props): JSX.Element | null {
     const loadImage = (src: string): Promise<HTMLImageElement> => {
         return new Promise((resolve, reject) => {
             const img = new Image();
-            img.crossOrigin = 'Anonymous'; // CORS desteği için
             img.onload = () => resolve(img);
             img.onerror = reject;
             img.src = src;
