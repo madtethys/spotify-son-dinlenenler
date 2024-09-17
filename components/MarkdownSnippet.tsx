f3d5cc225342c8a431d51db30fe7233d3a25a7ee
Mustafa Arda Düşova <info@madgrafik.com>
2024-09-17 11:22:09 +0300

    Instagram'da Paylaşma Denemesi

--- components/MarkdownSnippet.tsx
+++ components/MarkdownSnippet.tsx
@@ -11,26 +11,24 @@ const instagramToken = Constants.InstagramAppToken;
 interface Props {
     username?: string;
     theme: string;
-    instagramToken?: string; // Opsiyonel hale getirildi
+    instagramToken?: string;
 }
 
 export default function MarkdownSnippet(props: Props): JSX.Element | null {
     const { username, theme, instagramToken } = props;
-    const [trackCount, setTrackCount] = useState<number>(5); // Varsayılan değeri 5
-    const [width, setWidth] = useState<number>(400); // Varsayılan değeri 400
-    const [uniqueTracks, setUniqueTracks] = useState<boolean>(false); // Varsayılan değeri hayır
+    const [trackCount, setTrackCount] = useState<number>(5);
+    const [width, setWidth] = useState<number>(400);
+    const [uniqueTracks, setUniqueTracks] = useState<boolean>(false);
 
     if (!username) {
         return null;
     }
 
-    // SVG URL'sini ve parametreleri güncelle
     const svgSrc = `${Constants.BaseUrl}/api?user=${username}`;
     const updateParams = `&count=${trackCount}&width=${width}${uniqueTracks ? '&unique=true' : ''}`;
-    const markdownCode = `![Spotify Son Dinlenen Müzikler](${svgSrc}${updateParams})`;
-    const htmlCode = `<img src="${svgSrc}${updateParams}" alt="Spotify Son Dinlenen Müzikler - Mustafa Arda Düşova" />`;
+    const imageUrl = `${svgSrc}${updateParams}`;
+    const backgroundImageUrl = 'https://images.hdqwalls.com/download/landscape-reflection-lake-trees-in-1080x1920.jpg';
 
-    // Handle değişim fonksiyonları
     const handleWidthChange = (value: number | [number, number]) => {
         if (typeof value === 'number') {
             setWidth(value);
@@ -43,28 +41,45 @@ export default function MarkdownSnippet(props: Props): JSX.Element | null {
         }
     };
 
-    // Instagram API üzerinden paylaşım fonksiyonu
-    const shareToInstagramStory = async () => {
-        const imageUrl = `${svgSrc}${updateParams}`;
+    const combineImages = async () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const img1 = new Image();
+        const img2 = new Image();
+
+        img1.src = backgroundImageUrl;
+        img2.src = imageUrl;
+
+        img1.onload = () => {
+            canvas.width = img1.width;
+            canvas.height = img1.height;
+            ctx?.drawImage(img1, 0, 0);
+
+            img2.onload = () => {
+                ctx?.drawImage(img2, 50, 50, img1.width - 100, img1.height - 100); // İkinci resmi biraz küçük ve merkezde göstereceğiz
+                const finalImage = canvas.toDataURL('image/png');
+                shareToInstagramStory(finalImage); // Instagram API'ye gönder
+            };
+        };
+    };
+
+    const shareToInstagramStory = async (finalImage: string) => {
         if (!instagramToken) {
             alert('Instagram token eksik. Görsel paylaşımı yapılamaz.');
-            console.log('Instagram Token:', process.env.REACT_APP_INSTAGRAM_TOKEN);
             return;
         }
         try {
-            // Medya nesnesi oluştur
             const mediaResponse = await axios.post(
                 `https://graph.facebook.com/v16.0/{page_id}/media`,
                 {
-                    image_url: imageUrl,
-                    caption: "Your caption here",
+                    image_url: finalImage,
+                    caption: "Spotify Son Dinlenen Müzikler",
                     access_token: instagramToken,
                 }
             );
 
             const mediaId = mediaResponse.data.id;
 
-            // Medya nesnesini yayınla
             await axios.post(
                 `https://graph.facebook.com/v16.0/{page_id}/media_publish`,
                 {
@@ -82,119 +97,9 @@ export default function MarkdownSnippet(props: Props): JSX.Element | null {
 
     return (
         <div>
-            <Title level={5} style={{ color: theme === 'dark' ? '#e0e0e0' : '#222222', marginBottom: '20px' }}>
-                👤 "{username}" olarak giriş yapıldı.
-            </Title>
-            <Tabs defaultActiveKey="1">
-                <TabPane tab="❓ Markdown'a Nasıl Eklerim?" key="1">
-                    <Space className="vert-space" direction="vertical" size="small">
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            Markdown'a eklemek için kodunuz:
-                        </Title>
-                        <Text style={{ color: theme === 'dark' ? '#e0e0e0' : '#434242', fontSize: '14px' }}>
-                            ℹ️ Lütfen bu kodu markdown dosyanızda eklemek istediğiniz yere ekleyin.
-                        </Text>
-                        <TextArea
-                            className="markdown"
-                            autoSize
-                            readOnly
-                            value={markdownCode}
-                            style={{
-                                backgroundColor: theme === 'dark' ? '#333333' : '#ffffff',
-                                color: theme === 'dark' ? '#e0e0e0' : '#222222'
-                            }}
-                        />
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            spotify.mdusova.com - Önizleme:
-                        </Title>
-                        <img
-                            src={`${svgSrc}${updateParams}`}
-                            alt="Spotify Son Dinlenen Müzikler"
-                            key={updateParams} // Key özelliğini ekleyerek her değişimde yeniden render edilmesini sağlıyoruz
-                        />
-                    </Space>
-                </TabPane>
-                <TabPane tab="❓ HTML'e Nasıl Eklerim?" key="2">
-                    <Space className="vert-space" direction="vertical" size="small">
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            HTML'e eklemek için kodunuz:
-                        </Title>
-                        <Text style={{ color: theme === 'dark' ? '#e0e0e0' : '#434242', fontSize: '14px' }}>
-                            ℹ️ Lütfen bu kodu HTML kodunuzda eklemek istediğiniz yere ekleyin.
-                        </Text>
-                        <TextArea
-                            className="htmlkodu"
-                            autoSize
-                            readOnly
-                            value={htmlCode}
-                            style={{
-                                backgroundColor: theme === 'dark' ? '#333333' : '#ffffff',
-                                color: theme === 'dark' ? '#e0e0e0' : '#222222'
-                            }}
-                        />
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            spotify.mdusova.com - Önizleme:
-                        </Title>
-                        <img
-                            src={`${svgSrc}${updateParams}`}
-                            alt="Spotify Son Dinlenen Müzikler"
-                            key={updateParams} // Key özelliğini ekleyerek her değişimde yeniden render edilmesini sağlıyoruz
-                        />
-                    </Space>
-                </TabPane>
-                <TabPane tab="⚙️ Ayarları Yapılandırın" key="3">
-                    <Space className="vert-space" direction="vertical" size="small">
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            📋 Listede Bulunacak Müzik Sayısı:
-                        </Title>
-                        <Text style={{ color: theme === 'dark' ? '#e0e0e0' : '#434242', fontSize: '14px' }}>
-                            ℹ️ Listede bulunan müizk sayısını bu ayar ile ayarlayabilirsiniz. <br /> Minimum değer: 1 / Maksimum değer: 10 (Varsayılan değer: 5) <br />API URL'sine <b>&count=girdiğinizdeğer</b> ekleyecektir.
-                        </Text>
-                        <Tooltip title={`${trackCount} müzik`}>
-                            <Slider
-                                min={1}
-                                max={10}
-                                step={1}
-                                value={trackCount}
-                                onChange={handleTrackCountChange}
-                                className="slider"
-                            />
-                        </Tooltip>
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            ↔️ Listenin Genişliği(px):
-                        </Title>
-                        <Text style={{ color: theme === 'dark' ? '#e0e0e0' : '#434242', fontSize: '14px' }}>
-                            ℹ️ Listenizin genişliğini bu ayar ile ayarlayabilirsiniz. <br /> Minimum değer: 300 / Maksimum değer: 1000 (Varsayılan değer: 400) <br />API URL'sine <b>&width=girdiğinizdeğer</b> ekleyecektir.
-                        </Text>
-                        <Tooltip title={`${width}px`}>
-                            <Slider
-                                min={300}
-                                max={1000}
-                                step={1}
-                                value={width}
-                                onChange={handleWidthChange}
-                                className="slider"
-                            />
-                        </Tooltip>
-                        <Title level={5} style={{ color: theme === 'dark' ? '#ffffff' : '#222222' }}>
-                            🔁 Tekrar Dinlenen Müzikler:
-                        </Title>
-                        <Text style={{ color: theme === 'dark' ? '#e0e0e0' : '#434242', fontSize: '14px' }}>
-                            ℹ️ Listede tekrar dinlediğiniz müzikleri bu ayar ile gösterebilirsiniz. <br /> Gösterilsin veya gösterilmesin şeklindedir. Varsayılan olarak gösterilmeyecek şekilde ayarlıdır. <br />"Gösterilsin"i seçerseniz; API URL'sine <b>&unique=true</b> ekleyecektir. <br />"Gösterilmesin"i seçtiyseniz API URL'sine herhangi bir eklemek yapılmayacaktır.
-                        </Text>
-                        <Switch
-                            checked={uniqueTracks}
-                            onChange={checked => setUniqueTracks(checked)}
-                            checkedChildren="Gösterilsin"
-                            unCheckedChildren="Gösterilmesin"
-                            className="switch"
-                        />
-                    </Space>
-                </TabPane>
-            </Tabs>
-            {/* Paylaşım butonu ekleyelim */}
-            <Button onClick={shareToInstagramStory} type="primary" style={{ marginTop: 20 }}>
-                Instagram Stories'e Paylaş
+            {/* Diğer içerikler */}
+            <Button onClick={combineImages} type="primary" style={{ marginTop: 20 }}>
+            Instagram Hikayende Paylaş
             </Button>
         </div>
     );
